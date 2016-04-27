91fc6df1fdbcfb1a38d191ea7400f9b228775520
AmirrezaDadi <rezaamir2006@gmail.com>
2016-04-27 05:30:03 +0430

    offline quize (single player)

--- question_manager/static/question_manager/question.js
+++ question_manager/static/question_manager/question.js
@@ -1,3 +1,23 @@
+//var questionModule = angular.module('QuestionApp', []);
+//questionModule.factory('Questions', function($http) {
+//    var q = this;
+//    $http.get("./questions.json").success(function (response){
+//        q.questions = response.data;
+//    });
+//
+//    return {
+//        get_by_id: function(id){
+//            if(id < q.questions.length){
+//                q.questions[id].Choices.shuffle();
+//                return q.questions[id];
+//
+//            }
+//            else{
+//                return false;
+//            }
+//        }
+//    };
+//});
 Array.prototype.shuffle = function() {
     var input = this;
 
@@ -10,36 +30,91 @@ Array.prototype.shuffle = function() {
         input[i] = itemAtIndex;
     }
     return input;
-}
+};
 
-function QuestionController(Questions, $scope){
-    $scope.reset = function() {
-        $scope.inProgress = false;
-        $scope.score = 0;
-    };
-    $scope.reset();
-    $scope.start = function(){
-        $scope.inProgress = true;
-        $scope.id = 0;
-        $scope.score = 0;
-        $scope.question = Questions.get_by_id($scope.id);
-
-    };
-    $scope.checkAnwer = function(answer){
-        if(answer === Questions.get_by_id($scope.id).answer){
-            $scope.score++;
-        }
-        $scope.nextQuestion();
-        console.log($scope.id);
-    };
-    $scope.nextQuestion = function(){
-        $scope.id++;
-        $scope.question = Questions.get_by_id($scope.id);
-
-        if($scope.question === false){
+(function(){
+    var app = angular.module('singlePlayerQuiz',[]);
+    app.controller('QuestionController', function($scope, $http){
+        $scope.questions = []
+        $http.get("./questions.json").success(function(response){
+            $scope.questions = response;
+        });
+        $scope.reset = function() {
             $scope.inProgress = false;
-            $scope.finish = true;
-        }
-    };
-}
+            $scope.score = 0;
+            $scope.finish = false;
+        };
+        $scope.reset();
+        $scope.start = function(){
+            $scope.inProgress = true;
+            $scope.id = 0;
+            $scope.score = 0;
+            $scope.questions.shuffle();
+            $scope.questions = $scope.questions.slice(1,5);
+            $scope.question = $scope.questions[$scope.id].fields || false//Questions.get_by_id(this.id);
+            $scope.question.choices = [ {"id":1, "text": $scope.question.right_answer} , {"id":2, "text" : $scope.question.choice1}, {"id":3, "text": $scope.question.choice2},
+                {"id":4, "text": $scope.question.choice3}];
+            console.log($scope.question.choices);
+            $scope.question.choices.shuffle();
 
+        };
+        $scope.checkAnswer = function(answer){
+            if(answer === 1 ){//$scope.questions[$scope.id].fields.right_answer){//Questions.get_by_id($scope.id).answer){
+                $scope.score++;
+            }
+            $scope.nextQuestion();
+        };
+        $scope.nextQuestion = function(){
+            $scope.id++;
+            $scope.question = ($scope.questions[$scope.id] && $scope.questions[$scope.id].fields) || false//Questions.get_by_id($scope.id);
+            $scope.question.choices = [ {"id":1, "text": $scope.question.right_answer} , {"id":2, "text" : $scope.question.choice1}, {"id":3, "text": $scope.question.choice2},
+                {"id":4, "text": $scope.question.choice3}];
+            console.log($scope.question);
+            if($scope.question === false){
+                $scope.inProgress = false;
+                $scope.finish = true;
+            }
+        };
+    });
+})();
+
+
+//
+//function QuestionController($http, $scope){
+//    var q = this;
+//    q.questions = [];
+//    $http.get("./questions.json").success(function (response){
+//        q.questions = response;
+//        console.log(q.questions);
+//    });
+//    $scope.reset = function() {
+//        $scope.inProgress = false;
+//        $scope.score = 0;
+//    };
+//    $scope.reset();
+//    $scope.start = function(){
+//        $scope.inProgress = true;
+//        $scope.id = 0;
+//        $scope.score = 0;
+//        $scope.question = this.questions[$scope.id]//Questions.get_by_id($scope.id);
+//        console.log($scope.question)
+//
+//    };
+//    $scope.checkAnwer = function(answer){
+//        if(answer === questions[$scope.id].answer){//Questions.get_by_id($scope.id).answer){
+//            $scope.score++;
+//        }
+//        $scope.nextQuestion();
+//        console.log($scope.id);
+//    };
+//    $scope.nextQuestion = function(){
+//        $scope.id++;
+//        $scope.question = questions[$scope.id]//Questions.get_by_id($scope.id);
+//
+//
+//        if($scope.question === false){
+//            $scope.inProgress = false;
+//            $scope.finish = true;
+//        }
+//    };
+//}
